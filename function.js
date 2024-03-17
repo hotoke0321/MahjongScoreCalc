8e34bcc2ac670cd521ce5f714036294908e1e611
hotoke0321 <106749340+hotoke0321@users.noreply.github.com>
2024-03-17 14:45:50 +0900

    Update function.js

--- function.js
+++ function.js
@@ -1,20 +1,8 @@
-// The function here takes the parameters that you
-// have declared in the `glide.json` file, in the
-// same order.
-window.function = function (str, start, end) {
-  // For each parameter, its `.value` contains
-  // either its value in the type you've declared,
-  // or it's `undefined`.  This is a good place to
-  // extract the `.value`s and assign default
-  // values.
-  str = str.value ?? "";
-  start = start.value ?? 0;
-  end = end.value;
-
-  // Your function should return the exact type
-  // you've declared for the `result` in
-  // `glide.json`, or `undefined` if there's an
-  // error or no result can be produced, because a
-  // required input is `undefined`, for example.
-  return str.substring(start, end);
+window.function = function (PlayerNumber,DefTenbo,ReturnTenbo,RankPoint1,RankPoint2,RankPoint3,RankPoint4
+,Player1_Tenbo,Player2_Tenbo,Player3_Tenbo,Player4_Tenbo) {
+  if(PlayerNumber == 3){
+    return "sannma"
+  }else{
+    return "yonma"
+  }
 }
