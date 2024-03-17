4914fa4825296c645c19ff16d1cfb5eb0c44774f
hotoke0321 <106749340+hotoke0321@users.noreply.github.com>
2024-03-17 14:53:06 +0900

    Update function.js

--- function.js
+++ function.js
@@ -1,8 +1,8 @@
 window.function = function (PlayerNumber,DefTenbo,ReturnTenbo,RankPoint1,RankPoint2,RankPoint3,RankPoint4
 ,Player1_Tenbo,Player2_Tenbo,Player3_Tenbo,Player4_Tenbo) {
   if(PlayerNumber == 3){
-    return "sannma"
+    return "sannma";
   }else{
-    return "yonma"
+    return PlayerNumber;
   }
 }
